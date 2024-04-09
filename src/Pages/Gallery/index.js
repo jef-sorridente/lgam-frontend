9a5b7f40b7e640ead16e30b4698877e662ea5c43
Jeferson Soares <jpadilha53@gmail.com>
2024-04-09 18:13:42 -0300

    galley

--- src/Pages/Gallery/index.js
+++ src/Pages/Gallery/index.js
@@ -7,17 +7,80 @@ const Gallery = () => {
       <div className="gallery">
         <h3 className="title">Galeria</h3>
         <div className="gallery-container-image">
-          <div className="gallery-image">
-            <p>Torneio...</p>
+          <div className="gallery-card">
+            <div className="gallery-date">
+              <div className="gallery-date-title">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+              <div className="gallery-date-card">
+                <h4 className="gallery-date-num">26</h4>
+                <p>Março</p>
+              </div>
+            </div>
             <img src={image} alt="Lutadores" />
+            <div className="gallery-date-descri">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
           </div>
-          <div className="gallery-image">
-            <p>Torneio...</p>
+          <div className="gallery-card">
+            <div className="gallery-date">
+              <div className="gallery-date-title">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+              <div className="gallery-date-card">
+                <h4 className="gallery-date-num">26</h4>
+                <p>Março</p>
+              </div>
+            </div>
             <img src={image} alt="Lutadores" />
+            <div className="gallery-date-descri">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
           </div>
-          <div className="gallery-image">
-            <p>Torneio...</p>
+          <div className="gallery-card">
+            <div className="gallery-date">
+              <div className="gallery-date-title">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+              <div className="gallery-date-card">
+                <h4 className="gallery-date-num">26</h4>
+                <p>Março</p>
+              </div>
+            </div>
             <img src={image} alt="Lutadores" />
+            <div className="gallery-date-descri">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <div className="gallery-card">
+            <div className="gallery-date">
+              <div className="gallery-date-title">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+              <div className="gallery-date-card">
+                <h4 className="gallery-date-num">26</h4>
+                <p>Março</p>
+              </div>
+            </div>
+            <img src={image} alt="Lutadores" />
+            <div className="gallery-date-descri">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <div className="gallery-card">
+            <div className="gallery-date">
+              <div className="gallery-date-title">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+              <div className="gallery-date-card">
+                <h4 className="gallery-date-num">26</h4>
+                <p>Março</p>
+              </div>
+            </div>
+            <img src={image} alt="Lutadores" />
+            <div className="gallery-date-descri">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
           </div>
         </div>
       </div>
