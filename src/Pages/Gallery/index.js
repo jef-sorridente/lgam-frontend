119b684141d71a4ab9ec4022f533268e5821eea6
jef-sorridente <jpadilha53@gmail.com>
2024-04-10 00:50:43 -0300

    Estilização em gellery.css

--- src/Pages/Gallery/index.js
+++ src/Pages/Gallery/index.js
@@ -82,6 +82,21 @@ const Gallery = () => {
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
           </div>
+          <div className="gallery-card">
+            <div className="gallery-date">
+              <div className="gallery-date-title">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+              <div className="gallery-date-card">
+                <h4 className="gallery-date-num">26</h4>
+                <p>Março</p>
+              </div>
+            </div>
+            <img src={image} alt="Lutadores" />
+            <div className="gallery-date-descri">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
         </div>
       </div>
     </article>
